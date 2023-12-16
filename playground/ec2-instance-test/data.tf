75ff2560a50325fb0a0db61a93da8768b1ada1b6
Paul Buetow <paul@buetow.org>
2023-12-16 11:16:31 +0200

    add data.tf

---
+++ playground/ec2-instance-test/data.tf
@@ -0,0 +1,10 @@
+# Get latest Amazon Linux 2 AMI
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
