98c265971b82df3ee42776701ad27d847840c385
Paul Buetow <paul@buetow.org>
2023-12-18 10:15:10 +0200

    add more outputs

--- org-buetow-base/outputs.tf
+++ org-buetow-base/outputs.tf
@@ -1,3 +1,27 @@
 output "my_self_hosted_services_efs_id" {
   value = aws_efs_file_system.my_self_hosted_services_efs.id
 }
+
+output "my_public_subnet_a" {
+  value = aws_subnet.my_public_subnet_a.id
+}
+
+output "my_public_subnet_b" {
+  value = aws_subnet.my_public_subnet_b.id
+}
+
+output "my_public_subnet_c" {
+  value = aws_subnet.my_public_subnet_c.id
+}
+
+output "allow_ssh_sg_id" {
+  value = aws_security_group.allow_ssh.id
+}
+
+output "allow_web_sg_id" {
+  value = aws_security_group.allow_web.id
+}
+
+output "allow_outbound_sg_id" {
+  value = aws_security_group.allow_outbound.id
+}
