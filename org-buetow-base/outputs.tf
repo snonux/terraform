75660a7e4504b9ba13c3b0453664f1468a9682c8
Paul Buetow <paul@buetow.org>
2023-12-18 10:17:10 +0200

    rename outputs

--- org-buetow-base/outputs.tf
+++ org-buetow-base/outputs.tf
@@ -2,15 +2,15 @@ output "my_self_hosted_services_efs_id" {
   value = aws_efs_file_system.my_self_hosted_services_efs.id
 }
 
-output "my_public_subnet_a" {
+output "my_public_subnet_a_id" {
   value = aws_subnet.my_public_subnet_a.id
 }
 
-output "my_public_subnet_b" {
+output "my_public_subnet_b_id" {
   value = aws_subnet.my_public_subnet_b.id
 }
 
-output "my_public_subnet_c" {
+output "my_public_subnet_c_id" {
   value = aws_subnet.my_public_subnet_c.id
 }
 
