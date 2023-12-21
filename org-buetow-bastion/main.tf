1100359c0d0fbc26ee747bc6313a80373dbab1f3
Paul Buetow <paul@buetow.org>
2023-12-21 06:48:58 +0200

    use Amazon Linux 2023

--- org-buetow-bastion/main.tf
+++ org-buetow-bastion/main.tf
@@ -21,10 +21,10 @@ resource "aws_eip" "bastion" {
 }
 
 resource "aws_instance" "bastion" {
-  ami           = data.aws_ami.amazon_linux.id
+  ami           = "ami-024f768332f080c5e" # Amazon Linux 2023
   instance_type = "t2.micro"
   key_name      = aws_key_pair.id_rsa_pub.key_name
-  subnet_id     = data.terraform_remote_state.base.outputs.public_subnet_b_id
+  subnet_id     = data.terraform_remote_state.base.outputs.public_subnet_a_id
 
   vpc_security_group_ids = [
     data.terraform_remote_state.base.outputs.allow_ssh_sg_id,
@@ -33,6 +33,14 @@ resource "aws_instance" "bastion" {
   ]
   user_data = data.template_file.user_data.rendered
 }
+
+#resource "aws_route53_record" "bastion_ec2_buetow_cloud" {
+#  zone_id = data.terraform_remote_state.base.outputs.buetow_cloud_zone_id
+#  name    = "bastion-ec2.buetow.cloud"
+#  type    = "A"
+#  ttl     = "300"
+#  records = [aws_instance.bastion.public_ip]
+#}
 
 resource "aws_route53_record" "bastion_buetow_cloud" {
   zone_id = data.terraform_remote_state.base.outputs.buetow_cloud_zone_id
--- org-buetow-nextcloud/main.tf
+++ org-buetow-nextcloud/main.tf
@@ -11,38 +11,14 @@ provider "aws" {
   region = "eu-central-1" # or your preferred AWS region
 }
 
-data "aws_region" "current" {}
-
-data "template_file" "user_data" {
-  template = file("${path.module}/user_data.tpl")
-
-  vars = {
-    region = data.aws_region.current.name
-    efs_id = data.terraform_remote_state.base.outputs.self_hosted_services_efs_id
-  }
-}
-
-# Get latest Amazon Linux 2 AMI
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
 resource "aws_key_pair" "id_rsa_pub" {
   key_name   = "nextcloud-id-rsa-pub"
   public_key = file("${path.module}/id_rsa.pub")
 }
 
 resource "aws_instance" "nextcloud" {
-  ami           = data.aws_ami.amazon_linux.id
+  ami           = "ami-024f768332f080c5e" # Amazon Linux 2023
+
   instance_type = "t2.medium"
   key_name      = aws_key_pair.id_rsa_pub.key_name
   subnet_id     = data.terraform_remote_state.base.outputs.public_subnet_a_id
