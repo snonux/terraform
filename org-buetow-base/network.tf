a8ee6769039da289a842e72eacc72caffdf17f05
Paul Buetow <paul@buetow.org>
2023-12-23 12:14:51 +0200

    IPv6 works for EC2

--- org-buetow-base/network.tf
+++ org-buetow-base/network.tf
@@ -76,10 +76,11 @@ resource "aws_security_group" "allow_ssh" {
   vpc_id      = aws_vpc.vpc.id
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
