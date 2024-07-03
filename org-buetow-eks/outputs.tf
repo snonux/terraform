c65285ec170a778e97171e12257df05c0dd95869
Paul Buetow <paul@buetow.org>
2024-07-03 23:12:46 +0300

    add update-kubeconfig command as an output

--- org-buetow-eks/outputs.tf
+++ org-buetow-eks/outputs.tf
@@ -10,3 +10,6 @@ output "cluster_certificate_authority_data" {
   value = aws_eks_cluster.org_buetow_eks.certificate_authority.0.data
 }
 
+output "update_kubeconfig_command" {
+  value = "aws eks update-kubeconfig --name ${aws_eks_cluster.org_buetow_eks.name} --region eu-central-1"
+}
